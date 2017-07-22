b706ca3c01446ab25609451285d9ae8556851270
Lucas Trindade <lucasktrindade@gmail.com>
2017-07-22 15:48:28 -0300

    Adicionando rotas da aplicação e fazendo request para server do Cielo

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
 const request = require('request');
-request({
-    'url': 'https://apisandbox.cieloecommerce.cielo.com.br/1/sales/', //URL to hit
-    'method': 'POST',
-    'headers': {
+const app = require('express')();
+var bodyParser = require('body-parser');
+
+const environment = {
+    production: false,
+    cielo: {
+        RequestAPI: 'https://apisandbox.cieloecommerce.cielo.com.br',
+        QueryAPI: 'https://apiquerysandbox.cieloecommerce.cielo.com.br',
+        MerchantId: '312d414f-878b-4a6e-874b-b655f1badcc3',
+        MerchantKey: 'WJAKMEEKMGHDNRSBFREFZPFMADOPUNUMVFRPUZAV',
+    }
+};
+
+let options = {
+    headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        "MerchantId": "312d414f-878b-4a6e-874b-b655f1badcc3",
-        "MerchantKey": "WJAKMEEKMGHDNRSBFREFZPFMADOPUNUMVFRPUZAV",
-    },
-    //Lets post the following key/values as form
-    'json': {
-        "MerchantOrderId": "2014111703",
-        "Customer": {
-            "Name": "Comprador crédito simples"
-        },
-        "Payment": {
-            "Type": "CreditCard",
-            "Amount": 100,
-            "Installments": 1,
-            "SoftDescriptor": "123456789ABCD",
-            "CreditCard": {
-                "CardNumber": "0000.0000.0000.0001",
-                "Holder": "Teste Holder",
-                "ExpirationDate": "12/2030",
-                "SecurityCode": "123",
-                "SaveCard": "true",
-                "Brand": "Visa"
-            }
-        }
+        "MerchantId": environment.cielo.MerchantId,
+        "MerchantKey": environment.cielo.MerchantKey
     }
-}, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(response.statusCode, body);
-    }
-    res.status(200).send("this is a test");
-});
+}
+
+app.use(bodyParser.json());
+
+app.post('/pay', (req, res) => {
+    request({
+        "url": `${environment.cielo.RequestAPI}/1/sales/`,
+        "method": 'POST',
+        "headers": options.headers,
+        "json": req.body,
+    }, (error, response, body) => {
+        res.json(response)
+    })
+})
+
+app.get('/pay/:id', (req, res) => {
+    request({
+        "url": `${environment.cielo.QueryAPI}/1/sales/${req.params.id}`,
+        "method": "GET",
+        "headers": options.headers,
+    }, (error, response, body) => {
+        res.json(response)
+    })
+})
+
+app.put('/pay/:id/:amount', (req, res) => {
+    request({
+        "url": `${environment.cielo.RequestAPI}/1/sales/${req.params.id}/void?amount=${req.params.amount}`,
+        "method": "PUT",
+        "headers": options.headers,
+    }, (error, response, body) => {
+        res.json(response)
+    })
+})
+
+
+
+app.listen(3000);
