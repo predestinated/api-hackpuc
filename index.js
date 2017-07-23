9eff5b5d8e8230ae086a90d71dddf042246cef86
Allison Verdam da Cunha <borntoplay189@gmail.com>
2017-07-23 06:23:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const request = require('request');
+var cors = require('cors')
 const app = require('express')();
+app.use(cors())
 var bodyParser = require('body-parser');
 
 const environment = {
